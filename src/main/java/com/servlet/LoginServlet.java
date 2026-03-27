a0fb954d15a27eae8f3c98f93539abe2f263f780
Kumar-Aditya-Pratap <128625213+Kumar-Aditya-Pratap@users.noreply.github.com>
2026-03-27 11:48:03 +0530

    [Aditya Pratap] Added : UC4 - password validation

--- src/main/java/com/servlet/LoginServlet.java
+++ src/main/java/com/servlet/LoginServlet.java
@@ -17,18 +17,30 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         response.setContentType("text/html");
-
         PrintWriter out = response.getWriter();
 
         // UC3: Name Validation
         if(!name.matches("[A-Z][a-zA-Z]{2,}")) {
 
             out.println("<h3>Invalid Name</h3>");
-            out.println("Name must start with a capital letter and have at least 3 characters.");
+            out.println("Name must start with a capital letter and contain at least 3 characters.");
             return;
         }
 
-        if(name.equals("Admin") && password.equals("1234")) {
+        // UC4: Password Validation
+        if(!password.matches("^(?=.*[A-Z])(?=.*\\d)(?=[^@#$%^&*!]*[@#$%^&*!][^@#$%^&*!]*$).{8,}$")) {
+
+            out.println("<h3>Invalid Password</h3>");
+            out.println("Password must contain:");
+            out.println("<br>Minimum 8 characters");
+            out.println("<br>At least 1 uppercase letter");
+            out.println("<br>At least 1 number");
+            out.println("<br>Exactly 1 special character");
+            return;
+        }
+
+        // Login Check
+        if(name.equals("Admin") && password.equals("Admin@123")) {
 
             request.setAttribute("user", name);
 
